e4a97c989b15b5c51ccb8c62093d144704265d8e
Sabarya05 <sabaryajnvclt29@gmail.com>
2024-10-05 02:49:56 +0530

    Initial commit

---
+++ library/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Navbar.css'
+import clglogo from '../../assets/clglogo.png'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <img src="clglogo" alt="" />
+        <ul>
+            <li></li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
